45ab4aabc6612be72162c9943fa5b8177fdeaca6
Marcel Hild <hild@b4mad.net>
2017-02-01 11:31:15 +0100

    rubocop -a

--- app/models/manageiq/providers/amazon/inventory/collectors/network_manager.rb
+++ app/models/manageiq/providers/amazon/inventory/collectors/network_manager.rb
@@ -21,7 +21,8 @@ class ManageIQ::Providers::Amazon::Inventory::Collectors::NetworkManager < Manag
 
   def health_check_members(load_balancer_name)
     hash_collection.new(aws_elb.client.describe_instance_health(
-      :load_balancer_name => load_balancer_name).instance_states)
+      :load_balancer_name => load_balancer_name
+    ).instance_states)
   end
 
   def floating_ips
